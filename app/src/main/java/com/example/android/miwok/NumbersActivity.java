e7e6c52ee236d3581a1d7f429eacf43c7a571d85
Gustavo Berman <gustavoberman@gmail.com>
2016-07-17 13:03:12 -0300

    agregado el for

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,8 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,10 +17,28 @@ public class NumbersActivity extends AppCompatActivity {
         ArrayList<String> words = new ArrayList<String>();
         words.add("one");
         words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
+        // Find the root view so we can add child views to it
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+
+        for(int index=0;index < words.size(); index++){
+            // Create a new TextView
+            TextView wordView = new TextView(this);
+            // Set the text to be word at the current index
+            wordView.setText(words.get(index));
+            // Add this TextView as another child to the root view of this layout
+            rootView.addView(wordView);
+        }
     }
-
-
 
 
 }
